47269d4631b631eb1d11b8b00a578de8e966f063
muhammadalief19 <aliefmuhammad1908@gmail.com>
2022-09-02 06:31:58 +0700

    menambahkan fitur navbar fixed

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,15 @@ hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
 });
+
+// navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector('#head');
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+  } else {
+    header.classList.remove('navbar-fixed');
+  }
+};
